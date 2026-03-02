39fba94c71a23d59407474909d30a7cbc9083603
Rens Oliemans <hallo@rensoliemans.nl>
2026-03-02 13:31:18 +0100

    Improve webpage

--- app/main.js
+++ app/main.js
@@ -4,18 +4,29 @@ async function showPrimes(event) {
     const n = event.target.elements['n'].value;
 
     try {
+	showResponse(n, []);
+	showStatus(`Computing result...`);
 	const response = await fetch(`/api/primes-until/${n}`);
 	const body = await response.json();
 	const primes = body.primes;
 	showResponse(n, primes);
     } catch (e) {
 	console.error(e);
+	showStatus(`Failed to compute result.`);
     }
 }
 
+function showStatus(status) {
+    const intro = document.querySelector("#primes-intro");
+    intro.innerText = status;
+}
+
 function showResponse(n, primes) {
-    const displayElement = document.querySelector("#display-primes");
-    displayElement.innerText = `Primes lower than ${n}: ${primes}`;
+    const intro = document.querySelector("#primes-intro");
+    intro.innerText = `Primes lower than ${n}:`;
+
+    const sequence = document.querySelector("#sequence");
+    sequence.innerText = primes.join(', ');
 }
 
 form.addEventListener("submit", (event) => {
